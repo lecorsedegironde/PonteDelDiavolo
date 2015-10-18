ea6636e65fca801a3225f69575a1331fa5a20b5f
Bastien Hoffstetter <bastien.hoffstetter@free.fr>
2015-10-18 19:33:57 +0200

    Message Adversaire, début gestion plateau

--- src/fr/bhoffstetter/ponte/Partie.java
+++ src/fr/bhoffstetter/ponte/Partie.java
@@ -7,35 +7,33 @@ import java.util.Random;
 
 /**
  * @author Olivier
- *
- * gestion d'une partie
- *
- * aléatoire
- *
+ *         <p>
+ *         gestion d'une partie
+ *         <p>
+ *         aléatoire
  */
 public class Partie {
-
-    static int nbLignesColonnes; // dimension (carrée) du plateau
-
-    private static Random rnd = new Random();
 
     public final static char carPose2Pions = '+'; // caractère de pose de 2 pions
     public final static char carPose1Pont = '-'; // caractère de pose d'1 pont
     public final static char carArret = 'a'; // caractère d'arrêt
-
+    static int nbLignesColonnes; // dimension (carrée) du plateau
+    private static Random rnd = new Random();
     private InputStream fluxEntree; // flux en entrée depuis le serveur
     private OutputStream fluxSortie; // flux en sortie vers le serveur
 
-    @SuppressWarnings("unused")
+
     private boolean estPremierJoueur; // indique si le joueur joue en premier (ou en second sinon)
     private boolean couleurAChoisir; // indique si la couleur des pions est encore à choisir (en début de partie, au 2e coup) lorsque l'on est le second joueur
     private boolean couleurCLAIR; // couleur des pions
 
-    //Tableau qui stocke le plateau
+    //Tableau qui stocke le plateau --> 0 si clair, 1 si foncé, 2 si rien, si il y a un pont : LCcouleurPion et 3 si espace bloqué
     private int tableauJeu[][];
+    //private
 
     /**
      * gestion d'une partie
+     *
      * @param fluxEntree
      * @param fluxSortie
      * @param nbLignesColonnes
@@ -45,13 +43,19 @@ public class Partie {
         this.fluxSortie = fluxSortie;
         Partie.nbLignesColonnes = nbLignesColonnes;
         tableauJeu = new int[nbLignesColonnes][nbLignesColonnes];
+        for (int i = 0; i < nbLignesColonnes; i++) {
+            for (int j = 0; j < nbLignesColonnes; j++) {
+                tableauJeu[i][j] = 2;
+            }
+        }
     }
+
     /**
      * gestion d'une partie
      */
     public void partie() {
         boolean fini = false;
-        while (! fini) {
+        while (!fini) {
             int codeRetourArbitre = reception1CodeRetourArbitre();
             switch (codeRetourArbitre) {
                 case (int) 'P':
@@ -79,7 +83,7 @@ public class Partie {
                     break;
                 case (int) carArret:
                     // notre adversaire arrête ; c'est notre dernier coup si on a les pions foncés
-                    if (! couleurCLAIR) {
+                    if (!couleurCLAIR) {
                         envoiAction(action());
                     }
                     break;
@@ -120,6 +124,7 @@ public class Partie {
 
     /**
      * réception d'un code du serveur
+     *
      * @return codeRetourArbitre
      */
     private int reception1CodeRetourArbitre() {
@@ -132,30 +137,56 @@ public class Partie {
         }
         return codeRetourArbitre;
     }
+
     /**
      * réception de l'action de l'adversaire
-     * @param codeRetourArbitre
+     *
+     * @param codeRetourArbitre La première ligne
      * @return indique si ça s'est bien passé
      */
     //TODO gérer réception code de retour arbitre
     private boolean receptionAction(int codeRetourArbitre) {
+        int L1 = codeRetourArbitre, C1, L2, C2;
+        char played;
+
+        //Récupération première colone
+        C1 = reception1CodeRetourArbitre();
+        if (C1 == -1) {
+            return false;
+        }
+        //Récupération pion ou pont
+        played = (char) reception1CodeRetourArbitre();
         if (reception1CodeRetourArbitre() == -1) {
             return false;
         }
-        if (reception1CodeRetourArbitre() == -1) {
+        //Réception deuxième ligne
+        L2 = reception1CodeRetourArbitre();
+        if (L2 == -1) {
             return false;
         }
-        if (reception1CodeRetourArbitre() == -1) {
+        //Réception deuxième colone
+        C2 = reception1CodeRetourArbitre();
+        if (C2 == -1) {
             return false;
         }
-        if (reception1CodeRetourArbitre() == -1) {
+
+        //On stocke l'action éffectuée par l'adversaire
+        if (played == carPose2Pions) {
+            tableauJeu[L1][C1] = couleurCLAIR ? 0 : 1;
+        } else if (played == carPose1Pont) {
+            tableauJeu[L1][C1] = L2 * 100 + C2 * 10 + (couleurCLAIR ? 0 : 1);
+            tableauJeu[L2][C2] = L1 * 100 + C1 * 10 + (couleurCLAIR ? 0 : 1);
+            bloquerCasesPont(L1, C1, L2, C2);
+        } else {
             return false;
         }
+
         return true;
     }
 
     /**
      * envoi de l'action au serveur
+     *
      * @param action
      */
     private void envoiAction(String action) {
@@ -171,6 +202,7 @@ public class Partie {
 
     /**
      * action (LC+LC ou LC-LC voire arrêt)
+     *
      * @return action
      */
     //TODO Implémenter IA
@@ -178,10 +210,24 @@ public class Partie {
         if (Math.random() < 0.05) {
             return Character.toString(carArret);
         } else {
-            return Integer.toString(rnd.nextInt(nbLignesColonnes)) + Integer.toString(rnd.nextInt(nbLignesColonnes)) + Character.toString((Math.random()<0.6?carPose2Pions:carPose1Pont)) + Integer.toString(rnd.nextInt(nbLignesColonnes)) + Integer.toString(rnd.nextInt(nbLignesColonnes));
+            return Integer.toString(rnd.nextInt(nbLignesColonnes)) + Integer.toString(rnd.nextInt(nbLignesColonnes)) + Character.toString((Math.random() < 0.6 ? carPose2Pions : carPose1Pont)) + Integer.toString(rnd.nextInt(nbLignesColonnes)) + Integer.toString(rnd.nextInt(nbLignesColonnes));
         }
     }
 
+    private void bloquerCasesPont(int L1, int C1, int L2, int C2) {
+        if (L1 == L2) {
+            if (C1 < C2) tableauJeu[L1][C1 + 1] = 3;
+            else tableauJeu[L1][C1 - 1] = 3;
+        } else if (C1 == C2) {
+            if (L1 < L2) tableauJeu[L1 + 1][C1] = 3;
+            else tableauJeu[L1 - 1][C1] = 3;
+        } else if (L1 > L2) {
+            //if ()
+        } else if (L1 < L2) {
+
+        }
+
+    }
 }
 
 
